9fdb5ded012b877282c017e523b20d57135ee0e2
ivhak <hakonseniver@yahoo.no>
2022-10-06 14:23:05 +0200

    Add license header

--- ldrb-gpu.hpp
+++ ldrb-gpu.hpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2022 Iver Håkonsen
+//
+// ldrb-gpu is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ldrb-gpu is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ldrb-gpu.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Authors: Iver Håkonsen <hakonseniver@yahoo.no
+
 #ifndef LDRB_GPU_HPP
 #include <string>
 #include "mfem.hpp"
