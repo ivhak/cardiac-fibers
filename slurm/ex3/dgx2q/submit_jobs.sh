1df0c6b839032f221c2b944751cbb48bb424abf1
ivhak <hakonseniver@yahoo.no>
2022-11-16 10:35:40 +0100

    Update slurm scripts

--- slurm/ex3/dgx2q/submit_jobs.sh
+++ slurm/ex3/dgx2q/submit_jobs.sh
@@ -2,9 +2,16 @@
 
 mkdir -p jobs
 
-sbatch --job-name ldrb-gpu-dgx2q-N1-n1  --nodes 1 --ntasks=1  run_dgx2q.sbatch
-sbatch --job-name ldrb-gpu-dgx2q-N1-n2  --nodes 1 --ntasks=2  run_dgx2q.sbatch
-sbatch --job-name ldrb-gpu-dgx2q-N1-n4  --nodes 1 --ntasks=4  run_dgx2q.sbatch
-sbatch --job-name ldrb-gpu-dgx2q-N1-n8  --nodes 1 --ntasks=8  run_dgx2q.sbatch
-sbatch --job-name ldrb-gpu-dgx2q-N1-n16 --nodes 1 --ntasks=16 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n1  --nodes 1 --ntasks=1  --gres=gpu:1 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n2  --nodes 1 --ntasks=2  --gres=gpu:1 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n4  --nodes 1 --ntasks=4  --gres=gpu:1 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n8  --nodes 1 --ntasks=8  --gres=gpu:1 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n16 --nodes 1 --ntasks=16 --gres=gpu:1 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n32 --nodes 1 --ntasks=32 --gres=gpu:1 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n64 --nodes 1 --ntasks=64 --gres=gpu:1 run_dgx2q.sbatch
+
+sbatch --job-name ldrb-gpu-dgx2q-N1-n8-gpu2  --nodes 1 --ntasks=8  --gres=gpu:2 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n16-gpu2 --nodes 1 --ntasks=16 --gres=gpu:2 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n32-gpu2 --nodes 1 --ntasks=32 --gres=gpu:2 run_dgx2q.sbatch
+sbatch --job-name ldrb-gpu-dgx2q-N1-n64-gpu2 --nodes 1 --ntasks=64 --gres=gpu:2 run_dgx2q.sbatch
 
