1fd7bcd4af6654eafcb60b7d75aa133b65ca1291
ivhak <hakonseniver@yahoo.no>
2022-12-15 13:57:31 +0100

    Update slurm scripts

--- slurm/ex3/mi100q/submit_jobs.sh
+++ slurm/ex3/mi100q/submit_jobs.sh
@@ -2,13 +2,4 @@
 
 mkdir -p jobs
 
-sbatch --job-name cardiac-fibers-mi100q-N1-n1  --fres=rsmi:1 --nodes 1 --ntasks=1  run_mi100q.sbatch
-sbatch --job-name cardiac-fibers-mi100q-N1-n2  --fres=rsmi:1 --nodes 1 --ntasks=2  run_mi100q.sbatch
-sbatch --job-name cardiac-fibers-mi100q-N1-n4  --fres=rsmi:1 --nodes 1 --ntasks=4  run_mi100q.sbatch
-sbatch --job-name cardiac-fibers-mi100q-N1-n8  --fres=rsmi:1 --nodes 1 --ntasks=8  run_mi100q.sbatch
-sbatch --job-name cardiac-fibers-mi100q-N1-n16 --fres=rsmi:1 --nodes 1 --ntasks=16 run_mi100q.sbatch
-
-sbatch --job-name cardiac-fibers-mi100q-N2-n2  --gres=rsmi:2 --nodes 1 --ntasks=2  run_mi100q.sbatch
-sbatch --job-name cardiac-fibers-mi100q-N2-n4  --gres=rsmi:2 --nodes 1 --ntasks=4  run_mi100q.sbatch
-sbatch --job-name cardiac-fibers-mi100q-N2-n8  --gres=rsmi:2 --nodes 1 --ntasks=8  run_mi100q.sbatch
-sbatch --job-name cardiac-fibers-mi100q-N2-n16 --gres=rsmi:2 --nodes 1 --ntasks=16 run_mi100q.sbatch
+sbatch --job-name cardiac-fibers-mi100q-N1-n1-g1 --gres=rsmi:1 --nodes 1 --ntasks=1  run_mi100q.sbatch
--- slurm/ex3/mi210q/submit_jobs.sh
+++ slurm/ex3/mi210q/submit_jobs.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env sh
 
-mkdir -p jobs 
+mkdir -p jobs
 
-sbatch --job-name cardiac-fibers-mi210q-N1-n128-g2 --nodes 1 --gres=rsmi:2 --ntasks=128 run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n64-g2  --nodes 1 --gres=rsmi:2 --ntasks=64  run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n32-g2  --nodes 1 --gres=rsmi:2 --ntasks=32  run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n16-g2  --nodes 1 --gres=rsmi:2 --ntasks=16  run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n8-g2   --nodes 1 --gres=rsmi:2 --ntasks=8   run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n4-g2   --nodes 1 --gres=rsmi:2 --ntasks=4   run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n2-g2   --nodes 1 --gres=rsmi:2 --ntasks=2   run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n1-g1   --nodes 1 --gres=rsmi:1 --ntasks=1   run_mi210q.sbatch
-
-sbatch --job-name cardiac-fibers-N2-n128-g4 --nodes 2 --gres=rsmi:2 --ntasks=128 run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-N2-n64-g4  --nodes 2 --gres=rsmi:2 --ntasks=64  run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-N2-n32-g4  --nodes 2 --gres=rsmi:2 --ntasks=32  run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-N2-n16-g4  --nodes 2 --gres=rsmi:2 --ntasks=16  run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-N2-n8-g4   --nodes 2 --gres=rsmi:2 --ntasks=8   run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-N2-n4-g4   --nodes 2 --gres=rsmi:2 --ntasks=4   run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N1-n1-g1 --nodes 1 --gres=rsmi:1 --ntasks=1 run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N1-n2-g2 --nodes 2 --gres=rsmi:2 --ntasks=2 run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N2-n4-g4 --nodes 2 --gres=rsmi:2 --ntasks=4 run_mi210q.sbatch
