20693f7c635383d880ad8635e09e8679256838de
ivhak <hakonseniver@yahoo.no>
2022-12-16 13:19:07 +0100

    Update slurm scripts

--- slurm/ex3/mi210q/submit_jobs.sh
+++ slurm/ex3/mi210q/submit_jobs.sh
@@ -2,6 +2,10 @@
 
 mkdir -p jobs
 
+sbatch --job-name cardiac-fibers-mi210q-N1-n8-g1 --nodes 1 --gres=rsmi:1 --ntasks=8 run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N1-n4-g1 --nodes 1 --gres=rsmi:1 --ntasks=4 run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N1-n2-g1 --nodes 1 --gres=rsmi:1 --ntasks=2 run_mi210q.sbatch
 sbatch --job-name cardiac-fibers-mi210q-N1-n1-g1 --nodes 1 --gres=rsmi:1 --ntasks=1 run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n2-g2 --nodes 2 --gres=rsmi:2 --ntasks=2 run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N2-n4-g4 --nodes 2 --gres=rsmi:2 --ntasks=4 run_mi210q.sbatch
+# sbatch --job-name cardiac-fibers-mi210q-N1-n1-g1 --nodes 1 --gres=rsmi:1 --ntasks=1 run_mi210q.sbatch
+# sbatch --job-name cardiac-fibers-mi210q-N1-n2-g2 --nodes 2 --gres=rsmi:2 --ntasks=2 run_mi210q.sbatch
+# sbatch --job-name cardiac-fibers-mi210q-N2-n4-g4 --nodes 2 --gres=rsmi:2 --ntasks=4 run_mi210q.sbatch
