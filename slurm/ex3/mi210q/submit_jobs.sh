ac8520d5c647c5f793a86d243eb356f37ca5f914
ivhak <hakonseniver@yahoo.no>
2022-12-19 10:42:43 +0100

    Update slurm scripts

--- slurm/ex3/mi210q/submit_jobs.sh
+++ slurm/ex3/mi210q/submit_jobs.sh
@@ -2,7 +2,9 @@
 
 mkdir -p jobs
 
-sbatch --job-name cardiac-fibers-mi210q-N1-n8-g1 --nodes 1 --gres=rsmi:1 --ntasks=8 run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n4-g1 --nodes 1 --gres=rsmi:1 --ntasks=4 run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n2-g1 --nodes 1 --gres=rsmi:1 --ntasks=2 run_mi210q.sbatch
-sbatch --job-name cardiac-fibers-mi210q-N1-n1-g1 --nodes 1 --gres=rsmi:1 --ntasks=1 run_mi210q.sbatch
+COMMIT=HEAD
+
+sbatch --job-name cardiac-fibers-mi210q-N1-n8-g1 --nodes 1 --gres=rsmi:1 --ntasks=8 --export=ALL,CARDIAC_FIBERS_COMMIT=${COMMIT} run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N1-n4-g1 --nodes 1 --gres=rsmi:1 --ntasks=4 --export=ALL,CARDIAC_FIBERS_COMMIT=${COMMIT} run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N1-n2-g1 --nodes 1 --gres=rsmi:1 --ntasks=2 --export=ALL,CARDIAC_FIBERS_COMMIT=${COMMIT} run_mi210q.sbatch
+sbatch --job-name cardiac-fibers-mi210q-N1-n1-g1 --nodes 1 --gres=rsmi:1 --ntasks=1 --export=ALL,CARDIAC_FIBERS_COMMIT=${COMMIT} run_mi210q.sbatch
